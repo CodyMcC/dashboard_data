969a17fd316de9625d2d115f6c47bd49974d3920
codymcco <codymccomber@gmail.com>
2020-06-03 12:11:52 -0700

    debugging

--- store_victron_mppt_data.py
+++ store_victron_mppt_data.py
@@ -109,7 +109,7 @@ def main():
     client.on_message = on_message
 
     print("Pre connect")
-    client.connect("http://192.168.10.172", 1883, 60)
+    client.connect("192.168.10.172", 1883, 60)
     print("post connect")
 
     # Blocking call that processes network traffic, dispatches callbacks and
